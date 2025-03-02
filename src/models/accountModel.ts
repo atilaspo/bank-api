aa64d4bdb329a6998a2a11da4eb387523314ac74
atilaspo <santiago.ortiz@hotmail.com.ar>
2025-03-02 11:00:54 +1100

    🗃️ Chore: Adapt account model for PostgreSQL

--- src/models/accountModel.ts
+++ src/models/accountModel.ts
@@ -1,9 +1,32 @@
-export type Account = {
-    readonly id: number;
-    owner: string;
-    balance: number;
-  };
-  
-  // temporary database
-  export const accounts: Account[] = [];
-  
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/database";
+
+export class Account extends Model {
+    public id!: number;
+    public owner!: string;
+    public balance!: number;
+}
+
+// Define the schema
+Account.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        owner: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        balance: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            defaultValue: 0,
+        },
+    },
+    {
+        sequelize,
+        tableName: "accounts",
+    }
+);
